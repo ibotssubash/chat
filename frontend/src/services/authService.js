1c55821b5266718cc5677e60396584e9ae91c5c1
ibotssubash <ibots.subash@gmail.com>
2026-03-27 16:05:30 +0530

    Fix backend API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://chat-6-dbw5.onrender.com/'
+const API_BASE_URL = 'https://chat-6-dbw5.onrender.com'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
